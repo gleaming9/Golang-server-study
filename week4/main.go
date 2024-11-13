14a680ba86370dbd0481d388e141d1b4e9a3f41a
gleaming9 <gleaming9@khu.ac.kr>
2024-11-13 15:30:46 +0900

    add main.go description

--- week4/main.go
+++ week4/main.go
@@ -1,9 +1,10 @@
 package main
 
 /*
-44 패키지에서는 기본적인 Middleware의 형태를 알아봅니다.
-45 패키지에서는 복원 미들웨어를 정의합니다.
-47 패키지에서는 request body를 로그에 남기는 미들웨어를 정의합니다.
+44 section은 기본적인 Middleware의 형태를 알아봅니다.
+45 section은 복원 미들웨어를 정의합니다.
+47 section은 request body를 로그에 남기는 미들웨어를 정의합니다.
+48 section은 로그를 남기는 미들웨어를 정의합니다.
 */
 func main() {
 
